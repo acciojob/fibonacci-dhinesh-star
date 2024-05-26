c8a1656906d448d7981b9468da432b879359dddc
Dhinesh Kumaran I M <77685230+dhinesh-star@users.noreply.github.com>
2024-05-26 06:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
-}
+	if(n==0) return 0;
+	else if(n==1) return 1;
+
+	let sum1 = fibonacci(num-1);
+	let sum2 = fibonacci(num-2);
+
+	return sum1+sum2;
+} 
 
 module.exports = fibonacci;
